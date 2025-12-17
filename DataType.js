b5fb7fcfd52f7b8b411732c10cdc1c68e51333ba
divya-balu24 <divi.tonami@gmail.com>
2025-12-17 17:09:54 +0530

    Add files via upload
    week1

---
+++ DataType.js
@@ -0,0 +1,13 @@
+var testerName = "Divya"
+var organizationname = null
+var contactNumber = 1237894562
+var lotteryNumber = 1237894562467n
+var isAutomationTester = true
+var hasPlaywrightExperience
+
+console.log("Tester name is " +testerName +typeof testerName)
+console.log("Organization name is  "+organizationname +typeof organizationname)
+console.log("Contact number is "+contactNumber +typeof contactNumber)
+console.log("Lottery number is "+lotteryNumber +typeof lotteryNumber)
+console.log("Is Automation Tester "+isAutomationTester +typeof isAutomationTester)
+console.log("had playwright Experience "+hasPlaywrightExperience +typeof hasPlaywrightExperience)
---
+++ Scoping.js
@@ -0,0 +1,15 @@
+
+var browserVersion = 149
+
+{
+    var browserName = "Firefox"
+    let browserVersion = 152
+    const loadUrl = "testbuddy.com"
+
+    console.log("IB Browser name is :"+browserName)
+    console.log("IB Browser version is :"+browserVersion)
+    console.log("IB URL is :"+loadUrl)
+}
+console.log("OB Browser name is :"+browserName)
+console.log("OB Browser version is :"+browserVersion)
+//console.log("OB URL is :"+loadUrl)
