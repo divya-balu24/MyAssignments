2ad5eb50fd810e119d77219bb4ec45cf6eea7ece
divya-balu24 <divi.tonami@gmail.com>
2025-12-17 17:21:01 +0530

    Create ifElseCond.js

---
+++ Day2/ifElseCond.js
@@ -0,0 +1,13 @@
+let mark=75;
+if(mark>=90){
+    console.log("Congratulations ! You got A Grade")
+}
+else if(mark>=80){
+    console.log("You got B Grade")
+}
+else if(mark>=70){
+    console.log("You got C grade")
+}
+else{
+    console.log("Sorry ! You are fail")
+}
