007b64f2e1ff99e9d8875d478435bb05bf3928eb
divya-balu24 <divi.tonami@gmail.com>
2025-12-18 18:03:29 +0530

    Create ReverseString.js

---
+++ Day3/ReverseString.js
@@ -0,0 +1,9 @@
+let word="Divya"
+let len = word.length;
+let revstr=""
+console.log(len)
+for(let i=len-1;i>=0;i--){
+     revstr= revstr+word.charAt(i)
+     console.log(revstr)
+}
+console.log(revstr)
