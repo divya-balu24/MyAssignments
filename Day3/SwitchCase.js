889fc89e1654b477664c78dbed4b3b471b5a0df4
divya-balu24 <divi.tonami@gmail.com>
2025-12-18 18:02:17 +0530

    Create SwitchCase.js

---
+++ Day3/SwitchCase.js
@@ -0,0 +1,22 @@
+let day = 4
+switch (day) {
+    case 1 :
+    console.log("It is Sunday")
+    break
+    case 2 :
+    console.log("It is Monday")
+    break
+    case 3 :
+    console.log("It is Tuesday")
+    break
+    case 4:
+    console.log("It is Wednesday")
+    break;
+    case 5:
+    console.log("It is Thursday")
+    break
+    default:
+    console.log("Day not in the list")
+    break
+
+}
